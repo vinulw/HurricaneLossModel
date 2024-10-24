e3e816739a06bd7c614c5909b464d582099469aa
Vinul W <vinul25@live.co.uk>
2024-10-24 14:37:38 +0100

    Added type checking to model.

--- hurricane_loss_model/model.py
+++ hurricane_loss_model/model.py
@@ -1,5 +1,6 @@
 import numpy as np
 from numpy.random import poisson, lognormal
+from collections.abc import Iterable
 
 
 class LocationProfile():
@@ -7,7 +8,8 @@ class LocationProfile():
     Wrapper class that contains information about the location.
     """
 
-    def __init__(self, landfall_rate, loss_prob_mean, loss_prob_stddev):
+    def __init__(self, landfall_rate: float, loss_prob_mean: float,
+                 loss_prob_stddev: float) -> None:
         self.landfall_rate = landfall_rate
         self.loss_prob_mean = loss_prob_mean
         self.loss_prob_stddev = loss_prob_stddev
@@ -18,11 +20,12 @@ class GenerateLossCalculation():
     Prepare and run total loss calculation for a single location.
     """
 
-    def __init__(self, location_profile, n_samples=1000):
+    def __init__(self, location_profile: LocationProfile,
+                 n_samples=1000) -> None:
         self.loc = location_profile
         self.n = n_samples
 
-    def run(self):
+    def run(self) -> float:
         num_events = poisson(self.loc.landfall_rate, self.n)
         loss_per_event = lognormal(mean=self.loc.loss_prob_mean,
                                    sigma=self.loc.loss_prob_stddev,
@@ -31,7 +34,8 @@ class GenerateLossCalculation():
         return np.sum(loss_per_event)
 
 
-def runLossCalculations(locations, n_samples):
+def runLossCalculations(locations: Iterable[LocationProfile],
+                        n_samples: int) -> float:
     """
     Calculate the average annual loss in $Billions for a list of locations.
     """
