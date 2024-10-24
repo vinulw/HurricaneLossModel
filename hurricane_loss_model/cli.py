51625bff6808ab88839ba8291e412b951b9e5170
Vinul W <vinul25@live.co.uk>
2024-10-24 12:04:20 +0100

    Implement command line parser
    - Modified project toml to allow for cli installation.
    - Implemented argument parsing according to spec.

---
+++ hurricane_loss_model/cli.py
@@ -0,0 +1,47 @@
+import argparse
+
+
+def parse_args():
+    """
+    Function to parse command line arguments.
+    """
+    parser = argparse.ArgumentParser(
+        prog='gethurricaneloss',
+        description="""
+            Calculates the average annual hurricane loss in $Billions for
+            a simple hurricane model.
+            """
+    )
+
+    requiredArguments = [
+        ('florida_landfall_rate',
+         'The anuual rate of landfalling hurricanes in Florida.'),
+        ('florida_mean',
+         '''The LogNormal parameters that describe the economic loss of
+         a landfalling hurricane in Florida.'''),
+        ('florida_stddev', ''),
+        ('gulf_landfall_rate',
+         'The annual rate of landfalling hurricanes in the Gulf state.'),
+        ('gulf_mean',
+         '''The LogNormal parameters that describe the economic loss of
+         a landfalling hurricane in the Gulf states.'''),
+        ('gulf_stddev',
+         '')
+    ]
+
+    for arg in requiredArguments:
+        parser.add_argument(arg[0], help=arg[1], default=1000, type=float)
+
+    # Add optional parameter
+    parser.add_argument('-n', '--num_monte_carlo_samples', type=int,
+                        help="""Number of samples (simulation years) to run
+                        (default 1000).""")
+
+    return parser.parse_args()
+
+
+def main():
+    """
+    Main function for CLI. It parses arguments and runs the model.
+    """
+    args = parse_args()
